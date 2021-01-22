cf7bc92306115ac73418799f36372bcccf506e42
hardikphalet <hardik.phalet@gmail.com>
2021-01-23 02:18:08 +0530

    Initial Commit

---
+++ Breakout/Breakout/Breakout.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <SDL.h>
+#include <SDL_mixer.h>
+#include <SDL_image.h>
+#include <string>
+#include <cmath>
+
+int main(int argc, char* args[])
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}
